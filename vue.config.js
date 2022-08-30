7809b5917d6c4a6a30c787be449f494956e66a71
DESKTOP-GPOOF7G\dell <842889009@qq.com>
2022-08-30 14:27:28 +0800

    ee

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,11 @@
 // var px2rem = require('postcss-px2rem')
 module.exports = {
   devServer: {
+    open: true, //配置自动启动浏览器
+    host: "localhost",
+    https: false,
+    hotOnly: false, //热更新
+    port: 80,
     //before: require('./mock/index.js') // 引入mock/index.js
     proxy: {
       // 匹配所有以 /api 开头的url
