313f834307bdea885fd9f35db91a147586f7679c
DESKTOP-GPOOF7G\dell <842889009@qq.com>
2022-09-05 10:19:19 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -33,8 +33,8 @@ module.exports = {
       },
       "/plcdata": {
         //
-        // target: "http://221.206.242.116:9000",
-        target: "http://10.168.1.176:9000",
+        target: "http://221.206.242.116:9000",
+        //target: "http://10.168.1.176:9000",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
